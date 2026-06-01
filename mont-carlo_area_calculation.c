554f709c9807adaf41b92d979ed741f1e8bdbcf2
MariaSabouri <mariasabouri3@gmail.com>
2026-06-01 22:09:07 +0200

    trying to work with scatterv

--- mont-carlo_area_calculation.c
+++ mont-carlo_area_calculation.c
@@ -1,6 +1,7 @@
+#include <ctime>
 #include <mpi.h>
 #include <stdio.h>
-#include <math.h>
+#include <time.h>
 /*
 If your code includes mathematical functions (like exp, cos, etc.), you need to link to the mathematics 
 library libm.so. This is done, just like for serial compiling, by adding -lm to the end of your compile command, 
@@ -8,7 +9,7 @@ that is,
 
 mpicc mont-carlo_area_calculation.c -o mont-carlo_area_calculation -lm
 */
-#define U(x) (sin(x))
+#define U(x) (sin(x) + 1)   //U(x) must be positive in my code 
 
 
 int mont_carlo(
@@ -20,14 +21,20 @@ void get_input(
     int rank_num,
     int comm_sz,
     double* a_p,
-    double* b_p
+    double* b_p,
+    int* n_p,
+    double* h
 ){
+    
     if (rank_num == 0){
         printf("Enter a, b\n");
-        scanf("%lf %lf", a_p, b_p);
+        scanf("%lf %lf %d", a_p, b_p, n_p);
     }
+    *h = (*b_p - *a_p) / *n_p;
+
     MPI_Bcast(a_p, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(b_p, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(n_p, 1, MPI_INT, 0, MPI_COMM_WORLD);
 }
 
 void distribute_data(
@@ -35,31 +42,49 @@ void distribute_data(
     int comm_size,
     double* a_p,
     double* b_p,
-    double* h,
+    int* n_p,
+    double* h_p,
     double* local_a,
-    double* local_b
+    double* local_b,
+    int* local_n,
+    int* send_counts,
+    int* displacements
 ){
-    *local_a = *a_p + rank_num * (*h);
-    *local_b = *local_a + (*h);
+    int sub_area;
+    
+    *local_n = *n_p / comm_size;
+    int remainder = *n_p % comm_size;
+    *local_n += (rank_num < remainder) ? 1 : 0;
+
+    if(rank_num == 0){
+        int offset = 0;
+        for (int i = 0; i< comm_size; i++) {
+            sub_area = *n_p / comm_size + ((i < remainder) ? 1 : 0);
+            send_counts[i] = sub_area;
+            displacements[i] = offset;
+            offset += sub_area;
+        }
+    } 
+
+    MPI_Bcast(send_counts, *n_p, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(displacements, *n_p, MPI_INT, 0, MPI_COMM_WORLD);
+
 }
     
 int main(void){
-    int comm_size, rank_num, n;
+    int comm_size, rank_num, n, local_n;
     double a, b, h, local_a, local_b;
+    int send_counts[comm_size], displacements[comm_size];
 
     MPI_Init(NULL,NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank_num);
 
-    get_input(rank_num, comm_size, &a, &b);
-    h = (b - a) / comm_size;
+    get_input(rank_num, comm_size, &a, &b, &n, &h);
 
-    distribute_data(rank_num, comm_size, &a, &b, &h, &local_a, &local_b);
+    distribute_data(rank_num, comm_size, &a, &b, &n, &h, &local_a, &local_b, &local_n, send_counts, displacements);
 
-
-    printf("rank: %d, local_a: %fl, local_b: %fl \n", rank_num, local_a, local_b);
-
-
+    
 
 
     MPI_Finalize();
